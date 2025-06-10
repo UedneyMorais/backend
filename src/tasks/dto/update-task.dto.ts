27a1ab5423dc00a59960a120b025fdd4432985be
Uedney Morais <uedneymorais@gmail.com>
2025-06-10 15:24:45 -0300

    Adjusted to run correctly

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,14 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  @IsNotEmpty()
-  title: string;
+  // Se 'title' for fornecido, ele deve ser uma string e não pode ser vazio
+  @IsOptional() // Permite que 'title' seja omitido na requisição
+  @IsString()
+  @IsNotEmpty() // Só valida se 'title' estiver presente e não for vazio
+  title?: string; // Torna a propriedade opcional no nível do TypeScript
 
-  @IsNotEmpty()
-  description: string;
+  // Se 'description' for fornecido, ele deve ser uma string e não pode ser vazio
   @IsOptional()
-  completed: boolean;
+  @IsString()
+  @IsNotEmpty() // Só valida se 'description' estiver presente e não for vazio
+  description?: string; // Torna a propriedade opcional no nível do TypeScript
+
+  // Se 'completed' for fornecido, ele deve ser um booleano
+  @IsOptional() // Permite que 'completed' seja omitido na requisição
+  @IsBoolean()
+  completed?: boolean; // Torna a propriedade opcional no nível do TypeScript
 }
